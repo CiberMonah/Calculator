2d65164f5daec57fe4693493bbae6a5adc1bcf85
CiberMonah <glisanov.andrej@yandex.ru>
2023-11-10 18:29:25 +0300

    Added base functions to processor

--- cpu_comands.cpp
+++ cpu_comands.cpp
@@ -1,4 +1,5 @@
 #include "cpu_comands.h"
+#include "commons.h"
 #include "stack.h"
 #include <math.h>
 
@@ -54,50 +55,84 @@ void comand_div(CPU* cpu) {
 }
 
 void comand_cos(CPU* cpu) {
-    Elem_t res = 0, number = 0;
-    comand_pop(stk, &number);
-    res = (int)(cos(double(number)));
-    comand_push(stk, res);
+    int res = 0;
+    int number = 0;
+    stack_pop(&cpu->stk, &number);
+    res = cos((double)number);
+    stack_push(&cpu->stk, res);
 }
 
 void comand_sin(CPU* cpu) {
-    Elem_t res = 0, number = 0;
-    comand_pop(stk, &number);
-    res = (int)(sin(double(number)));
-    comand_push(stk, res);
+    int res = 0;
+    int number = 0;
+    stack_pop(&cpu->stk, &number);
+    res = sin((double)number);
+    stack_push(&cpu->stk, res);
 }
 
 void comand_sqrt(CPU* cpu) {
-    Elem_t res = 0, number = 0;
-    comand_pop(stk, &number);
-    res = (int)(sqrt((double)number));
-    comand_push(stk, (Elem_t) res);
+    int res = 0;
+    int number = 0;
+    stack_pop(&cpu->stk, &number);
+    res = sqrt((double)number);
+    stack_push(&cpu->stk, res);
 }
 
 void comand_rpush(CPU* cpu) {
-    Elem_t number = 0;
-    comand_pop(proc->stk, &number);
-    proc->rax
+    int number = 0;
+    stack_pop(&cpu->stk, &number);
+    cpu->car++;
+    int reg = cpu->comand_buffer[cpu->car];
+    switch (reg) {
+        case RAX:
+            cpu->rax = number;
+            break;
+        case RBX:
+            cpu->rbx = number;
+            break;
+        case RCX:
+            cpu->rcx = number;
+            break;
+        case RDX:
+            cpu->rdx = number;
+            break;
+        default:
+            break;
+    }
 }
 
 void comand_rpop(CPU* cpu) {
-    Elem_t number = 0;
-    comand_pop(proc->stk, &number);
-    comand_push(proc->stk, number);
-    proc->rax
+    cpu->car++;
+    int reg = cpu->comand_buffer[cpu->car];
+    switch (reg) {
+        case RAX:
+            stack_push(&cpu->stk, cpu->rax);
+            break;
+        case RBX:
+            stack_push(&cpu->stk, cpu->rbx);
+            break;
+        case RCX:
+            stack_push(&cpu->stk, cpu->rcx);
+            break;
+        case RDX:
+            stack_push(&cpu->stk, cpu->rdx);
+            break;
+        default:
+            break;
+    }
 }
 
 
 void comand_in(CPU* cpu) {
     printf("Input number - \n");
-    Elem_t number = 0;
+    int number = 0;
     scanf("%d", &number);
-    comand_push(stk, number);
+    stack_push(&cpu->stk, number);
 }
 
 void comand_out(CPU* cpu) {
-    Elem_t number = 0;
-    comand_pop(stk, &number);
-    printf("%d", number);
+    int number = 0;
+    stack_pop(&cpu->stk, &number);
+    printf("%d ", number);
 }
 
